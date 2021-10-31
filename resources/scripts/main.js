f11724ad2b1a6ee77df98cec0ee1b20eb1a256c2
IsaacBernardino <isaacbernardino@gmail.com>
2021-10-31 18:05:25 -0300

    REV 1.3110 | Primeira versão finalizada

---
+++ resources/scripts/main.js
@@ -0,0 +1,64 @@
+// Elementos do html
+
+// slider
+const projetoEl = document.querySelector('.projeto');
+
+const counterEl =  document.querySelector('#counter');
+
+// botoes
+const nextButtonEl = document.querySelector('#next');
+const prevButtonEl = document.querySelector('#prev');
+
+let imageIndex = 0;
+
+const imagens = [
+    'resources/images/screenshots/manager.jpg',
+    'resources/images/screenshots/manager2.jpg',
+    'resources/images/screenshots/manager3.jpg',
+
+    'resources/images/screenshots/stopwatch.jpg',
+    'resources/images/screenshots/stopwatch1.jpg',
+    'resources/images/screenshots/stopwatch2.jpg',
+
+    'resources/images/screenshots/todo.jpg',
+    'resources/images/screenshots/todo2.jpg',
+    'resources/images/screenshots/todo3.jpg'
+]
+
+nextButtonEl.addEventListener('click', nextImage);
+prevButtonEl.addEventListener('click', prevImage);
+
+function updateSlider (){
+  requestAnimationFrame(updateSlider);
+
+  projetoEl.src = imagens[imageIndex];
+}
+
+function nextImage (){
+  if(imageIndex < imagens.length - 1){
+    imageIndex++;
+  }
+}
+
+function prevImage(){
+  if(imageIndex > 0){
+    imageIndex--;
+  }
+}
+
+updateSlider()
+counterEl.innerHTML = `${imagens.length} Imagens`;
+// botoes de contato
+document.querySelector('#goto-git').addEventListener('click',() => {
+  window.open('https://github.com/IsaacBernardino');
+});
+
+
+document.querySelector('#goto-linke').addEventListener('click',() => {
+  window.open('https://www.linkedin.com/in/isaac-bernardino-24884a121/');
+});
+
+document.querySelector('#goto-fb').addEventListener('click',() => {
+  window.open('https://www.facebook.com/isaacbernadino7');
+});
+
